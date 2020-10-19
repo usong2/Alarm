a7c5c0f4e8d74c529b0ac670178cc66652d6976b
usong2 <dbthd6@naver.com>
2020-10-19 16:39:53 +0900

    alarm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { of, interval, concat, Subject } from 'rxjs';
+import { takeWhile, takeUntil, scan, startWith, repeatWhen, share, filter } from 'rxjs/operators';
+// import './styles.css';
+
+const countdown$ = interval(200).
+    pipe(
+        startWith(5),
+        scan(time => time - 1),
+        takeWhile(time => time > 0)
+    ).pipe(share());
+
+const action$ = new Subject();
+const snooze$ = action$.pipe(filter(action => action === 'snooze'));
+const dismiss$ = action$.pipe(filter(action => action === 'dismiss'));
+
+const snoozeableAlarm$ = concat(countdown$, of('Wake up! 🎉')).pipe(repeatWhen(() => snooze$));
+
+const observable$ = concat(snoozeableAlarm$.pipe(
+    takeUntil(dismiss$)
+), of('Have a nice day! 🎃'));
 
 function App() {
+  const [state, setState] = useState();
+  useEffect(() => {
+      const sub = observable$.subscribe(setState);
+      return () => sub.unsubscribe();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <h3>Alarm Clock</h3>
+          <div className="display">{state}</div>
+          <button className="snooze" onClick={() => action$.next('snooze')}>Snooze</button>
+          <button className="dismiss" onClick={() => action$.next('dismiss')}>Dismiss</button>
+      </>
   );
 }
 
